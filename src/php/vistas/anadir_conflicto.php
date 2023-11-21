3ca3e0b507cd808105e01cf39d9c89bcdb2ca9d0
Aura Janeiro Díaz <ajaneirodiaz.guadalupe@alumnado.fundacionloyola.net>
2023-11-21 12:50:21 +0100

    datos cambiado por fecha (aun no está dinámico)

--- src/php/vistas/anadir_conflicto.php
+++ src/php/vistas/anadir_conflicto.php
@@ -7,24 +7,46 @@
     <link rel="stylesheet" href="css/style_admin.css">
 </head>
 <body>
+    <nav class="navbar">
+        <h1>Añadir conflicto</h1>
+        <a class="back-link" href="index.php?controller=conflicto&action=menu">Volver</a>
+    </nav>
     <main>
         <div>
             <h1>Añadir conflictos</h1>
-            <form action="anadir_conflicto_exito.html" method="post">
+            <form action="index.php?controller=conflicto&action=insertar" method="post" enctype="multipart/form-data">
                     <label for="titulo">Título:</label>
                     <input type="text" name="titulo" placeholder="Introducir título">
                     <label for="informacion">Información:</label>
                     <textarea name="informacion" placeholder="Escribe aquí"></textarea>
-                    <label for="reflexion">Datos del incio del conflicto:</label>
-                    <textarea name="reflexion" placeholder="Escribe aquí"></textarea>
+                    <label for="fecha">Fecha de inicio del conflicto:</label>
+                    <input type="date" name="fecha"></input>
                     <label for="imagen">Imagen (opcional):</label>
                     <input type="file" name="imagen">
                     <div class="motivos">
                         <h2>Añadir motivos</h2>
                         <label for="motivos">Información:</label>
-                        <textarea name="motivos" placeholder="Escribe aquí"></textarea>      
+                        <textarea name="motivos[1]" placeholder="Escribe aquí"></textarea>      
                         <label>
-                            <input type="radio" id="motivo1" name="motivo" value="motivo1">
+                            <input type="radio" name="motivoCorrecto" value="motivo1">
+                            Es correcto
+                        </label>
+                    </div>
+                    <div class="motivos">
+                        <h2>Añadir motivos</h2>
+                        <label for="motivos">Información:</label>
+                        <textarea name="motivos[2]" placeholder="Escribe aquí"></textarea>      
+                        <label>
+                            <input type="radio" name="motivoCorrecto" value="motivo2">
+                            Es correcto
+                        </label>
+                    </div>
+                    <div class="motivos">
+                        <h2>Añadir motivos</h2>
+                        <label for="motivos">Información:</label>
+                        <textarea name="motivos[3]" placeholder="Escribe aquí"></textarea>      
+                        <label>
+                            <input type="radio" name="motivoCorrecto" value="motivo3">
                             Es correcto
                         </label>
                     </div>
