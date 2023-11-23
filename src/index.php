93a1fa0f8f288e432f4197767dab15e7be5c3eb6
Aura Janeiro Díaz <ajaneirodiaz.guadalupe@alumnado.fundacionloyola.net>
2023-11-23 10:57:08 +0100

    index cambiado

--- src/index.php
+++ src/index.php
@@ -4,28 +4,36 @@ require_once __DIR__.'/php/config/config.php';
 require_once __DIR__.'/php/config/configdb.php';
 
 
-if(!isset($_GET["controller"])) $_GET["controller"] = constant("DEFAULT_CONTROLLER");
-if(!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
+if(isset($_GET["controller"])) {
+    $nombreControlador = $_GET["controller"];
+} else{
+    //Si no se ha enviado un controlador por url se toma el controlador por defecto (el menú principal)
+    $nombreControlador = constant("DEFAULT_CONTROLLER");
+}
 
-$controller_path = __DIR__.'/php/controladores/'.$_GET["controller"].'.php';
+if(isset($_GET["action"])) {
+    $nombreMetodo = $_GET["action"];
+} else{
+    //Si no se ha enviado un método por url se toma el método que se encuentra como constante en el  (el menú principal)
+    $nombreMetodo = constant("DEFAULT_ACTION");
+}
 
-/* Check if controller exists */
-if(!file_exists($controller_path)) $controller_path = __DIR__.'/php/controladores/'.constant("DEFAULT_CONTROLLER").'.php';
+$rutaControlador = __DIR__.'/php/controladores/'.$nombreControlador.'.php';
 
-/* Load controller */
-require_once $controller_path;
-$controllerName = $_GET["controller"].'Controller';
-$controller = new $controllerName();
+// Carga el controlador
+require_once $rutaControlador;
+$nombreClaseControlador = $nombreControlador.'Controller';
+$controlador = new $nombreClaseControlador();
 
 /* Check if method is defined */
 
 $dataToView["data"] = array();
-if(method_exists($controller,$_GET["action"])) $dataToView["data"] = $controller->{$_GET["action"]}();
+if(method_exists($controlador,$nombreMetodo)) $dataToView["data"] = $controlador->{$nombreMetodo}();
 
 
 /* Load views */
 require_once __DIR__.'/php/vistas/template/header.php';
-require_once __DIR__.'/php/vistas/'.$controller->view.'.php';
+require_once __DIR__.'/php/vistas/'.$controlador->view.'.php';
 require_once __DIR__.'/php/vistas/template/footer.php';
 
 
