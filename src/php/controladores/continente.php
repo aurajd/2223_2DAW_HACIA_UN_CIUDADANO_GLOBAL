7cfa7efa794cbaa62429500a66a743c5d0d71fe2
Manuel Nieto Benítez <mnietobenitez.guadalupe@alumnado.fundacionloyola.net>
2023-11-27 12:58:40 +0100

    Continente

---
+++ src/php/controladores/continente.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Controlador que gestiona los contientes.
+ */
+class continenteController{
+
+    // Propiedades de la clase
+    
+    
+
+    /**
+     * Constructor de la clase continenteController.
+     *
+     *
+     * @return void
+     */
+    public function __construct() {
+        
+    }
+
+    
+}
---
+++ src/php/modelos/continente.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__.'/conexion.php';
+/**
+ * Clase continenteModel: Proporciona métodos para interactuar con la base de datos en relación con continentes.
+ */
+class continenteModel extends Conexion{
+    
+}
