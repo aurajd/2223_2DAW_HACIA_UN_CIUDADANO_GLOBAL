f57fc4ac5e916662c91dbfde991a16ada3517a0a
Manuel Nieto Benítez <mnietobenitez.guadalupe@alumnado.fundacionloyola.net>
2023-11-27 10:42:09 +0100

    continente, situacion, ranking.

--- sql/creacion_tablas.sql
+++ sql/creacion_tablas.sql
@@ -1,29 +1,52 @@
+CREATE TABLE continente (
+    idContinente TINYINT AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(17) NOT NULL,
+    informacion VARCHAR(2000) NOT NULL,
+    resumenInfo VARCHAR(100) NOT NULL,
+    imagen VARCHAR(18) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
 CREATE TABLE situacion (
-    idSituacion smallint unsigned AUTO_INCREMENT PRIMARY KEY,
+    idSituacion SMALLINT unsigned AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(50) NOT NULL,
     informacion VARCHAR(2000) NOT NULL,
     imagen CHAR(18) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE problema (
-    idProblema smallint unsigned PRIMARY KEY,
+    idProblema SMALLINT unsigned PRIMARY KEY,
     reflexion VARCHAR(2000) NOT NULL,
     CONSTRAINT fk_problema_situacion FOREIGN KEY (idProblema) REFERENCES situacion (idSituacion) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE conflicto(
-    idConflicto smallint unsigned PRIMARY KEY,
-    numMotivo tinyint DEFAULT NULL,
+    idConflicto SMALLINT unsigned PRIMARY KEY,
+    numMotivo TINYINT DEFAULT NULL,
     fechaInicio date NOT NULL,
     CONSTRAINT fk_conflicto_situacion FOREIGN KEY (idConflicto) REFERENCES situacion (idSituacion) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 CREATE TABLE motivo(
-    idConflicto smallint unsigned,
-    numMotivo tinyint,
+    idConflicto SMALLINT unsigned,
+    numMotivo TINYINT,
     textoMotivo VARCHAR(2000) NOT NULL,
     PRIMARY KEY(idConflicto, numMotivo),
     CONSTRAINT fk_motivo_conflicto FOREIGN KEY (idConflicto) REFERENCES situacion (idSituacion) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+CREATE TABLE solucion (
+    idSituacion SMALLINT unsigned,
+    numSolucion TINYINT NOT NULL,
+    textoSolucion VARCHAR(2000) NOT NULL,
+    correcta BIT NOT NULL,
+    PRIMARY KEY(idSituacion, numSolucion),
+    CONSTRAINT fk_solucion_situacion FOREIGN KEY (idSituacion) REFERENCES situacion (idSituacion) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;.
+
+CREATE TABLE ranking (
+    idPartida MEDIUMINT unsigned AUTO_INCREMENT PRIMARY KEY,
+    puntuacion CHAR(17) NOT NULL,
+    nombreJugador CHAR(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 ALTER TABLE conflicto
